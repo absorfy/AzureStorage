2030dfd20e5cc93de2be99eab7a5ddc5ef2b8868
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-09-15 14:49:05 +0300

    uploading files to azure storage

--- backend/src/main/java/my/backend/controller/FileController.java
+++ backend/src/main/java/my/backend/controller/FileController.java
@@ -1,7 +1,10 @@
 package my.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import my.backend.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +21,9 @@ public class FileController {
     @Autowired
     private FileService fileService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+    @Operation(summary = "Upload file")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upload(@Parameter(description = "File to upload") @RequestParam("file") MultipartFile file) {
         try {
             String url = fileService.uploadFile(file);
             return ResponseEntity.ok(url);
