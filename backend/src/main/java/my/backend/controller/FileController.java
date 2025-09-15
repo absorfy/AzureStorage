04302f99c19b10ac431856726d864f79834c5515
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-09-15 10:49:27 +0300

    fix controller upload method

--- backend/src/main/java/my/backend/controller/FileController.java
+++ backend/src/main/java/my/backend/controller/FileController.java
@@ -3,6 +3,7 @@ package my.backend.controller;
 import my.backend.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class FileController {
     @Autowired
     private FileService fileService;
 
+    @PostMapping("/upload")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
         try {
             String url = fileService.uploadFile(file);
